d0d8594db9b935a42d0d60f985ba7bab2ed895ea
yJLiGit <yijin.li.jack@gmail.com>
2015-07-26 15:29:22 -0400

    This is for R programminm-Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,44 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix: makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##To begin with, the inverse has not been cacluated
+        inverse <- NULL
+        ##if the matrix has been reset, inverse reset to null instead of using cache
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        ##retrieve the original matrix, if inverse == NULL, 
+        ##the inverse of the matrix has not been calculated or the matrix has been reset
+        ##else, use the cache
+        get <- function() x
+        ##calculate inverse of the matrix and save to cache 
+        setinverse <- function(ins) inverse <<- ins
+        ##get the inverse of the matrix from cache 
+        getinverse <- function() inverse
+                
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## cacheSolve :This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## if inverse has been set to cache , return the result from cache , else calculate the inverse of the matrix
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
